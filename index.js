8c9b4b2210fcb057543353921fdc1eff8da6f241
leapdating <80498709+leapdating@users.noreply.github.com>
2021-03-18 21:36:40 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,88 +1,73 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import GithubCorner from "react-github-corner";
+import React from "react";
+import PropTypes from "prop-types";
+import jsonp from "jsonp";
+import toQueryString from "to-querystring";
+import SimpleForm from "./SimpleForm";
 
-import MailchimpSubscribe from "../../src";
+const getAjaxUrl = url => url.replace("/post?", "/post-json?");
 
-// a basic form
-const CustomForm = ({ status, message, onValidated }) => {
-  let email, name;
-  const submit = () =>
-    email &&
-    name &&
-    email.value.indexOf("@") > -1 &&
-    onValidated({
-      EMAIL: email.value,
-      NAME: name.value
-    });
-
-  return (
-    <div
-      style={{
-        background: "#efefef",
-        borderRadius: 2,
-        padding: 10,
-        display: "inline-block"
-      }}
-    >
-      {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-      {status === "error" && (
-        <div
-          style={{ color: "red" }}
-          dangerouslySetInnerHTML={{ __html: message }}
-        />
-      )}
-      {status === "success" && (
-        <div
-          style={{ color: "green" }}
-          dangerouslySetInnerHTML={{ __html: message }}
-        />
-      )}
-      <input
-        style={{ fontSize: "2em", padding: 5 }}
-        ref={node => (name = node)}
-        type="text"
-        placeholder="Your name"
-      />
-      <br />
-      <input
-        style={{ fontSize: "2em", padding: 5 }}
-        ref={node => (email = node)}
-        type="email"
-        placeholder="Your email"
-      />
-      <br />
-      <button style={{ fontSize: "2em", padding: 5 }} onClick={submit}>
-        Submit
-      </button>
-    </div>
-  );
-};
-
-class Demo extends Component {
-  render() {
-    const url =
-      "https://jster.us7.list-manage.com/subscribe/post?u=ed40c0084a0c5ba31b3365d65&id=ec6f32bf5e";
-    return (
-      <div>
-        <h1>react-mailchimp-subscribe Demo</h1>
-        <GithubCorner href="https://github.com/revolunet/react-mailchimp-subscribe" />
-        <h2>Default Form</h2>
-        <MailchimpSubscribe url={url} />
-        <h2>Custom Form</h2>
-        <MailchimpSubscribe
-          url={url}
-          render={({ subscribe, status, message }) => (
-            <CustomForm
-              status={status}
-              message={message}
-              onValidated={formData => subscribe(formData)}
-            />
-          )}
-        />
-      </div>
+class MailchimpSubscribe extends React.Component {
+  state = {
+    status: null,
+    message: null
+  };
+  subscribe = data => {
+    const params = toQueryString(data);
+    const url = getAjaxUrl(this.props.url) + "&" + params;
+    this.setState(
+      {
+        status: "sending",
+        message: null
+      },
+      () =>
+        jsonp(
+          url,
+          {
+            param: "c"
+          },
+          (err, data) => {
+            if (err) {
+              this.setState({
+                status: "error",
+                message: err
+              });
+            } else if (data.result !== "success") {
+              this.setState({
+                status: "error",
+                message: data.msg
+              });
+            } else {
+              this.setState({
+                status: "success",
+                message: data.msg
+              });
+            }
+          }
+        )
     );
+  };
+  render() {
+    return this.props.render({
+      subscribe: this.subscribe,
+      status: this.state.status,
+      message: this.state.message
+    });
   }
 }
 
-render(<Demo />, document.querySelector("#demo"));
+MailchimpSubscribe.propTypes = {
+  render: PropTypes.func,
+  url: PropTypes.string.isRequired
+};
+
+MailchimpSubscribe.defaultProps = {
+  render: ({ subscribe, status, message }) => (
+    <SimpleForm
+      status={status}
+      message={message}
+      onSubmitted={formData => subscribe(formData)}
+    />
+  )
+};
+
+export default MailchimpSubscribe;
